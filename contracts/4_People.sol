7516652618e390c325bc5693c7f113056345ceb1
devon39 <christophe.jolas@gmail.com>
2023-05-18 10:51:13 +0200

    [TP2] contract people

--- contracts/4_People.sol
+++ contracts/4_People.sol
@@ -5,8 +5,13 @@ pragma solidity >=0.8.2 <0.9.0;
 contract People {
     struct Person {
         string name;
-        uint8 age;
+        uint age;
     }
 
     Person public moi;
+
+    function modifyPerson(string memory _name, uint _age ) public  {
+        moi.age = _age;
+        moi.name = _name;
+    }
 }
