2b4cbbbe815765f0e13fce73467d1d74b16351a1
devon39 <christophe.jolas@gmail.com>
2023-05-18 11:32:04 +0200

    [TP4] contract people

--- contracts/5_People1.sol
+++ contracts/5_People1.sol
@@ -9,4 +9,13 @@ contract People1 {
     }
 
     Person[] public persons;
+
+    function add (string memory _name, uint _age) public {
+        Person memory person = Person(_name, _age);
+        persons.push(person);
+    }
+
+    function remove() public {
+        persons.pop();
+    }
 }
