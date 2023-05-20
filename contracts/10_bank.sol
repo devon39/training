fefe73c1bf5d553f60272990b40e9ae7a5991516
devon39 <christophe.jolas@gmail.com>
2023-05-20 18:59:47 +0200

    [TP6] bank

---
+++ contracts/10_bank.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Bank {
+    mapping(address=> uint) private _balances;
+
+    function deposit(uint _amount) public {
+        _balances[msg.sender] += _amount;
+    }
+
+    function transfer(address _recipient, uint _amount) public {
+        require(_recipient != address(0), "You cannot transfer to the address zero");
+        require(_balances[msg.sender] >= _amount, "You have not enough balance");
+       _balances[_recipient] += _amount;
+       _balances[msg.sender] -= _amount;
+    }
+
+    function balanceOf(address _address) public view returns (uint){
+       return _balances[_address];
+    }
+}
