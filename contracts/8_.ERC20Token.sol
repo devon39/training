b8885703e357eaf031885a97fe917d862f43a967
devon39 <christophe.jolas@gmail.com>
2023-05-20 10:15:13 +0200

    [TP7] reused code

---
+++ contracts/8_.ERC20Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor() ERC20("GuestToGuestToken", "GTG") {
+    }
+}
+
