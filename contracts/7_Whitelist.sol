7b1445c4a9289b6b55b98e61f651842cc249477a
devon39 <christophe.jolas@gmail.com>
2023-05-20 09:12:58 +0200

    [TP5] modifier

---
+++ contracts/7_Whitelist.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Whitelist {
+   mapping(address=> bool) whitelist;
+   event Authorized(address _address); // Event
+ 
+    modifier check(){
+        require (whitelist[msg.sender]==true, "you are not authorized");
+        _;
+   }
+ 
+   function authorize(address _address) public check {
+       whitelist[_address] = true;
+       emit Authorized(_address); // Triggering event
+   }
+}
