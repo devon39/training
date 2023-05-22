d9ffbeefc0269539625e3bf84593e0fc4d9ff234
devon39 <christophe.jolas@gmail.com>
2023-05-22 09:36:39 +0200

    [TP9] Admin

---
+++ contracts/11_Admin.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Admin is Ownable{
+    mapping(address=> bool) private _whitelist;
+    mapping(address=> bool) private _blacklist;
+    event Whitelisted(address _address);
+    event Blacklisted(address _address);
+    
+    function whitelist(address _address) public onlyOwner {
+        require(!_whitelist[_address], "This address is already whitelisted !");
+        require(!_blacklist[_address], "This address is already blacklisted !");
+        _whitelist[_address] = true;
+        emit Whitelisted(_address);
+    }
+    
+    function isWhitelisted(address _address) public view onlyOwner returns (bool){
+        return _whitelist[_address];
+    }
+    function blacklist(address _address) public onlyOwner {
+        require(!_blacklist[_address], "This address is already blacklisted !");
+        require(!_whitelist[_address], "This address is already whitelisted !");
+        _blacklist[_address] = true;
+        emit Blacklisted(_address);
+    }
+    
+    function isBlacklisted(address _address) public view onlyOwner returns (bool){
+        return _blacklist[_address];
+    }
+}
